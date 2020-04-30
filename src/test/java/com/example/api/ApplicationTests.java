e3aee8d37d098cd824e7f85b4c5a92659f4e89c6
Jason Li <p9289213@gmail.com>
2020-04-30 23:12:37 +0800

    Add unit testing rest controller of get method.

--- src/test/java/com/example/api/ApplicationTests.java
+++ src/test/java/com/example/api/ApplicationTests.java
@@ -1,17 +1,40 @@
 package com.example.api;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import javax.annotation.Resource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests {
+@AutoConfigureMockMvc
+public class ApplicationTests{
+	@Resource
+	private MockMvc mvc;
 
 	@Test
-	public void contextLoads() {
-		System.out.println("執行單元測試");
+	public void getUsers() throws Exception {
+		String uri = "/rest/v1/users";
+		mvc.perform(get(uri)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content()
+						.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.[?(@.id==1)].username").value("Jason"))
+				.andExpect(jsonPath("$.[?(@.id==1)].password").value("1qaz2wsx"))
+				.andExpect(jsonPath("$.[?(@.id==1)].role").value("admin"));
 	}
-
 }
+
+
+
